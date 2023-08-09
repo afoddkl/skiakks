555b6cbefd2638480a8d8357ef2235390c458042
afoddkl <105531754+afoddkl@users.noreply.github.com>
2023-08-09 14:39:51 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
+UUID='c39e464a-13e7-46ed-86ae-5e759dbea5c7'
 VMESS_WSPATH='/vmess'
-VLESS_WSPATH='/vless'
+VLESS_WSPATH='/c39e464a-13e7-46ed-86ae-5e759dbea5c7-vless'
 TROJAN_WSPATH='/trojan'
 SS_WSPATH='/shadowsocks'
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" config.json
